dc4cacb3411533e04359aeacc9d888f2ef13a330
GoLeM-KD <kavija.codezela@gmail.com>
2026-03-14 18:41:59 +0530

    Created the back-end for the main page of Admin that created by Thilina at api>admin>userManagment only GET function and maded a Navbar for admins

---
+++ src/app/api/admin/userManagment/route.js
@@ -0,0 +1,50 @@
+import { queryDatabase } from "../../../db";
+import { cookies } from 'next/headers';
+
+export async function GET(req) {
+
+    try {
+
+        const { searchParams } = new URL(req.url);
+        const nameOrIDorUserName = searchParams.get("search") || "";
+        const selectedRole = searchParams.get("role") || "";
+        // We get the userID of the current admin to get users except that current admin
+        const cookieStore = await cookies();
+        const tokens = cookieStore.get('token')?.value;
+
+        let getUserQry = `
+            SELECT [UserId]
+                ,[FirstName]
+                ,[LastName]
+                ,[UserName]
+                ,[Email]
+                ,[Role]
+                ,[ImageURL]
+            FROM [dbo].[USER_MST]
+            WHERE [UserId] != 'UID00001'
+        `;
+
+        if(nameOrIDorUserName && !selectedRole) {
+
+            getUserQry += `AND [UserId] LIKE '%${nameOrIDorUserName}%' OR [UserName] LIKE '%${nameOrIDorUserName}%' OR [FirstName] LIKE '%${nameOrIDorUserName}%' OR [LastName] LIKE '%${nameOrIDorUserName}%'`;
+
+        } else if (!nameOrIDorUserName && selectedRole) {
+
+            getUserQry += `AND [Role] = '${selectedRole}'`;
+
+        } else if (nameOrIDorUserName && selectedRole) {
+
+            getUserQry +=`AND [UserId] LIKE '%${nameOrIDorUserName}%' OR [UserName] LIKE '%${nameOrIDorUserName}%' OR [FirstName] LIKE '%${nameOrIDorUserName}%' OR [LastName] LIKE '%${nameOrIDorUserName}%' AND [Role] = '${selectedRole}}'`;
+
+        }
+
+        const usersResult = await queryDatabase(getUserQry);
+
+        return new Response(JSON.stringify({users: usersResult}), {status: 200});
+
+    } catch (err) {
+
+        console.log("ADMIN GET USERS...", err);
+        return new Response(JSON.stringify({Error: err}), {status: 500});
+    } 
+}
